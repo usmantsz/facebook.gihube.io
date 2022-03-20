5d2529ea07d0e8c9b8ac863174d7ea57fc5317a1
DESKTOP-CG5TM6O\HP <usmanali0013691@gmail.com>
2022-03-20 05:37:28 +0500

    Updates

---
+++ precache-manifest.8b9c7f8e7306fd99eab43198e06a3f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86f7fcc72c15ca4c07b3d16e658346e",
+    "url": "/facebook.gihube.io/index.html"
+  },
+  {
+    "revision": "6a7fb8bf3be8f4290621",
+    "url": "/facebook.gihube.io/static/css/main.2fbb6b54.chunk.css"
+  },
+  {
+    "revision": "39987e1c756af5e697ea",
+    "url": "/facebook.gihube.io/static/js/2.f805aab5.chunk.js"
+  },
+  {
+    "revision": "abc28c365c609ffc9a2e2e165c7e73d8",
+    "url": "/facebook.gihube.io/static/js/2.f805aab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7fb8bf3be8f4290621",
+    "url": "/facebook.gihube.io/static/js/main.a61c8e60.chunk.js"
+  },
+  {
+    "revision": "a4ee94b38f220fd67e97",
+    "url": "/facebook.gihube.io/static/js/runtime-main.40e45e34.js"
+  }
+]);
